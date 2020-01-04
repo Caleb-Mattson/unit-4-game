7b86c224d3459851297f939cf69e347d23861fd5
CandyMan <c.mattson318@gmail.com>
2020-01-03 17:11:50 -0700

    populated image folder with necessary images. Deployed background image to page. Created Random Number function to call later in JS code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-});
+    // create variables to hold value information
+    var crystalValue = [];
+    var playerPoint = 0;
+    var targetPoint = '';
+
+    // create number randomizer to give crystals their value
+    function randomNumber(max, min){
+        var returnNumber;
+        returnNumber = Math.floor(Math.random() * (max-min) + min)
+       return returnNumber;    
+       }
+
+       $("clickMe").on("click", function(){
+        randomNumber(12, 1);
+        console.log(returnNumber);
+       });
+
+
+// });
